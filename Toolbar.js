b3a53feec44c4902c35f2fcea9cf950e6ba97039
ramiv212 <ramiv212@hotmail.com>
2023-02-22 23:07:44 -0600

    added a navbar (but with no functionality yet)

--- Toolbar.js
+++ Toolbar.js
@@ -1,28 +1,52 @@
 import { intToPx,pxToInt } from "./helpers.js"
 
+const button1Image = 'left-arrow.svg';
+const button2Image = 'right-arrow.svg';
+
+
 export class ToolbarButton {
-    constructor(parent,onClick) {
+    constructor(parent,buttonImage,description,onClick) {
         this.parent = parent;
 
-        this.div = document.createElement('button');
-        this.div.className = 'toolbar-button';
+        this.buttonImage = buttonImage;
+        this.description = description;
 
-        this.parent.append(this.div);
+        this.element = document.createElement('button');
+        this.element.className = 'toolbar-button';
+
+        this.parent.append(this.element);
+
+        // add passed down image to button
+        this.element.innerHTML = `<img src="/static/svgs/${this.buttonImage}" alt=${this.description} class="toolbar-button-img">`;
 
         // execute function passed down by user
-        this.div.addEventListener('click',() => {
+        this.element.addEventListener('click',() => {
             onClick();
         })
         
     }
 }
 
+
+class Navbar {
+    constructor(parent) {
+        this.parent = parent;
+
+        this.innerDiv = document.createElement('div');
+        this.navBarField = document.createElement('input');
+
+        this.parent.append(this.innerDiv);
+        this.innerDiv.append(this.navBarField);
+
+        this.innerDiv.className = 'navbar-inner-div';
+        this.navBarField.className = 'navbar-input-field';
+    }
+}
+
+
 export class Toolbar {
     constructor(parent,description) {
         this.parent = parent;
-
-        this.description = description;
-        this.buttonImage = '/static/svgs/left-arrow.svg';
 
         this.div = document.createElement('div');
         this.div.className = 'toolbar';
@@ -38,9 +62,11 @@ export class Toolbar {
 
 
         // create button image and append it to button div
-        this.buttonSVG = `<img src="${this.buttonImage}" alt=${this.alt} class="toolbar-button-img">`;
-        this.button1 = new ToolbarButton(this.innerDiv,() => console.log('click'));
-        this.button1.div.innerHTML = this.buttonSVG;
+        this.button1 = new ToolbarButton(this.innerDiv,button1Image,'Back',() => console.log('click'));
+        this.button1 = new ToolbarButton(this.innerDiv,button2Image,'Back',() => console.log('click'));
+
+        // create a navbar
+        this.navBar = new Navbar(this.innerDiv);
     }
 
     resize() {
